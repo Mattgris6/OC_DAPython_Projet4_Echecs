51758ddf4a86684216e24f79b99f3a5065846882
Matthieu Grison <mat.grison@gmail.com>
2021-09-15 16:57:33 +0200

    Ajout de commentaires

--- main_menu.py
+++ main_menu.py
@@ -3,12 +3,14 @@ from tkinter import *
 import os
 
 class MenuPrincipal():
+    """Home page"""
     def __init__(self):
         self.window = Tk()
         self.window.title('Gestionnaire de tournoi')
         self.window.geometry("300x200+0+0")
         self.label_menu = Label(self.window, text="Menu principal")
         self.label_menu.place(relx = 0.5, rely = 0.1, anchor=CENTER)
+        # We create all buttons we need for home page
         self.button_new_tournament = Button(
             self.window,
             text="Nouveau tournoi",
@@ -33,6 +35,7 @@ class MenuPrincipal():
             command=self.window.quit
             )
         self.button_quit.place(relx = 0.9, rely = 0.9, anchor=CENTER)
+        # Insert an image of chess
         image_file = os.path.abspath(os.path.dirname(__file__)) + r'/Ressources/image_menu.jpg'
         image_menu = PhotoImage(image_file)
         self.canvas_image = Canvas(self.window, width=100, height=100, bg='blue')
@@ -40,13 +43,17 @@ class MenuPrincipal():
         self.canvas_image.place(relx = 0.9, rely = 0.2, anchor=NE)
 
     def new_tournament(self):
+        """Run the page to create a new tournament"""
         showinfo("Tournoi", "Nouveau tournoi!")
 
     def historic(self):
+        """Show the page of Historic"""
         showinfo("Historique", "Historique!")
 
     def players(self):
+        """Show all players in base"""
         showinfo("Joueurs", "Joueurs!")
 
     def run(self):
+        """Show the home page"""
         self.window.mainloop()
