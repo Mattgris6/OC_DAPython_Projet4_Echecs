c5f09c638c84d9dc2d56d6a8846f67ae80916505
Matthieu Grison <mat.grison@gmail.com>
2021-11-07 11:41:48 +0100

    Regroupement des vues dans un répertoire view

--- ctrl_flake8.py
+++ ctrl_flake8.py
@@ -1,11 +1,5 @@
 import os
 
-os.system("flake8 controller.py --max-line-length 119 --format=html --htmldir=flake8_rapport")
-os.system("flake8 model.py --max-line-length 119 --format=html --htmldir=flake8_rapport")
-os.system("flake8 view_create_player.py --max-line-length 119 --format=html --htmldir=flake8_rapport")
-os.system("flake8 view_create_tournament.py --max-line-length 119 --format=html --htmldir=flake8_rapport")
-os.system("flake8 view_main_menu.py --max-line-length 119 --format=html --htmldir=flake8_rapport")
-os.system("flake8 view_players.py --max-line-length 119 --format=html --htmldir=flake8_rapport")
-os.system("flake8 view_rounds.py --max-line-length 119 --format=html --htmldir=flake8_rapport")
-os.system("flake8 view_tournament_menu.py --max-line-length 119 --format=html --htmldir=flake8_rapport")
-os.system("flake8 view_tournaments.py --max-line-length 119 --format=html --htmldir=flake8_rapport")
+os.system("flake8 controller.py --max-line-length 119 --format=html --htmldir=flake8_rapport\controller")
+os.system("flake8 model.py --max-line-length 119 --format=html --htmldir=flake8_rapport\model")
+os.system("flake8 view --max-line-length 119 --format=html --htmldir=flake8_rapport\view")
